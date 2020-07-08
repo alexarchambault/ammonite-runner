11796c182c94c954b22164354859988dd30c2955
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-07-08 16:39:33 +0200

    Update interface to 0.0.23 (#42)

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ import sbt.Keys.scalaVersion
 object Deps {
 
   def caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.2"
-  def coursierInterface = "io.get-coursier" % "interface" % "0.0.22"
+  def coursierInterface = "io.get-coursier" % "interface" % "0.0.23"
   def coursierLauncher = "io.get-coursier" %% "coursier-launcher" % "2.0.0-RC6-21"
   def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.3"
   def macroParadise = "org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full
