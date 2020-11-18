24eb60678ee9dff522ed56d583720d79342934a5
Scala Steward <me@scala-steward.org>
2020-11-18 02:05:00 +0100

    Update svm to 20.3.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -10,7 +10,7 @@ object Deps {
   def coursierLauncher = "io.get-coursier" %% "coursier-launcher" % "2.0.6"
   def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.5"
   def macroParadise = "org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full
-  def svm = "org.graalvm.nativeimage" % "svm" % "19.3.4"
+  def svm = "org.graalvm.nativeimage" % "svm" % "20.3.0"
   def utest = setting {
     val sv = scalaVersion.value
     val ver =
