1dfc90d74b9f7b4da39f4d61c87d555a57143b94
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-23 10:45:26 +0100

    Update coursier-launcher to 2.0.7 (#74)

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
 
   def caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.4"
   def coursierInterface = "io.get-coursier" % "interface" % "0.0.25"
-  def coursierLauncher = "io.get-coursier" %% "coursier-launcher" % "2.0.6"
+  def coursierLauncher = "io.get-coursier" %% "coursier-launcher" % "2.0.7"
   def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.5"
   def macroParadise = "org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full
   def svm = "org.graalvm.nativeimage" % "svm" % "19.3.4"
