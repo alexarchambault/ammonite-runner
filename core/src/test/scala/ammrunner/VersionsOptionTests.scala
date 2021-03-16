cb54fbd848529125bc87c19d3abc46fe326a4f35
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-03-16 16:15:16 +0100

    Match "Ammonite" / "Scala" in a case-insensitive way

--- core/src/test/scala/ammrunner/VersionsOptionTests.scala
+++ core/src/test/scala/ammrunner/VersionsOptionTests.scala
@@ -19,6 +19,45 @@ object VersionsOptionTests extends TestSuite {
       assert(result == expected)
     }
 
+    "both versions, case insensitive" - {
+      * - {
+        val content =
+          """#!/usr/bin/env amm
+            |// Scala 2.14.3, ammonite 4.1.2
+            |val n = 2
+            |""".stripMargin
+
+        val result = VersionsOption.fromScript(content.linesIterator)
+        val expected = VersionsOption(Some("4.1.2"), Some("2.14.3"))
+
+        assert(result == expected)
+      }
+      * - {
+        val content =
+          """#!/usr/bin/env amm
+            |// scala 2.14.3-RC2, Ammonite 4.1.2
+            |val n = 2
+            |""".stripMargin
+
+        val result = VersionsOption.fromScript(content.linesIterator)
+        val expected = VersionsOption(Some("4.1.2"), Some("2.14.3-RC2"))
+
+        assert(result == expected)
+      }
+      * - {
+        val content =
+          """#!/usr/bin/env amm
+            |// SCALA 2.14.3, AmmonIte 4.1.2-M1
+            |val n = 2
+            |""".stripMargin
+
+        val result = VersionsOption.fromScript(content.linesIterator)
+        val expected = VersionsOption(Some("4.1.2-M1"), Some("2.14.3"))
+
+        assert(result == expected)
+      }
+    }
+
     "only scala" - {
       val content =
         """#!/usr/bin/env amm
