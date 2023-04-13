42493c8e1e28e11c4214474c578be12b99c41325
Scala Steward <scala_steward@virtuslab.com>
2023-04-13 12:25:56 +0000

    Update coursier-launcher to 2.1.1

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
 
   def caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.6"
   def coursierInterface = "io.get-coursier" % "interface" % "1.0.12"
-  def coursierLauncher = "io.get-coursier" %% "coursier-launcher" % "2.0.16"
+  def coursierLauncher = "io.get-coursier" %% "coursier-launcher" % "2.1.1"
   def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.6"
   def macroParadise = "org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full
   def osLib = "com.lihaoyi" %% "os-lib" % "0.9.0"
