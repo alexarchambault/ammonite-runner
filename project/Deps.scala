28bef1a2f15ab57dcec5681a17d469e11d4888a5
Scala Steward <scala_steward@virtuslab.com>
2025-01-08 13:45:09 +0000

    Update interface to 1.0.27

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ import sbt.Keys.scalaVersion
 object Deps {
 
   def caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.6"
-  def coursierInterface = "io.get-coursier" % "interface" % "1.0.18"
+  def coursierInterface = "io.get-coursier" % "interface" % "1.0.27"
   def coursierLauncher = "io.get-coursier" %% "coursier-launcher" % "2.1.10"
   def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.6"
   def macroParadise = "org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full
