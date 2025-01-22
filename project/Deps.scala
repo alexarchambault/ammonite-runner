0079c8cea7b59a2d5c1eb66cdcf931ee15a92221
Scala Steward <scala_steward@virtuslab.com>
2025-01-22 14:10:35 +0000

    Update data-class to 0.2.7

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
   def caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.6"
   def coursierInterface = "io.get-coursier" % "interface" % "1.0.18"
   def coursierLauncher = "io.get-coursier" %% "coursier-launcher" % "2.1.10"
-  def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.6"
+  def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.7"
   def macroParadise = "org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full
   def osLib = "com.lihaoyi" %% "os-lib" % "0.9.0"
   def utest = "com.lihaoyi" %% "utest" % "0.8.1"
